f6210e8b3c0228c6924cca8474b5502a8d00b2a3
Muhammed Ceylan <muhammedceylan@Muhammed-MacBook-Pro.local>
2022-10-23 16:03:12 +0300

    fixes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,121 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="gamers">
+    <div class="gamer">
+    <h1>SEN</h1>
+    <div class="gamerNumberOfLives"><h2>%100</h2></div>
+  </div>
+  <div class="monster">
+    <h1>CANAVAR</h1>
+    <div class="monsterNumberOfLives"><h2>%100</h2></div>
+  </div>
+  </div>
+  <div class="buttons">
+    <button class="attack">Saldırı</button>
+    <button class="specialAttack" >Özel Saldırı</button>
+    <button class="firstAid">İlk yardım</button>
+    <button class="giveUp">Pes Et</button>
+  </div>
+  <div class="log">
+    <h3 class="logsText">Logs</h3>
+    <div class="result">
+    <div class="gamerResut">
+      <h3>Canavarın canının azalttın (8)</h3>
+    </div>
+    <div class="monsterResult">
+      <h3>Canavarın saldırısı (5)</h3>
+    </div>
+  </div>
+  
+</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  data(){
+    return{
+      caount:0
+    }
+  },
   props: {
     msg: String
   }
 }
 </script>
+<style>
+.gamers{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.gamer{
+  padding-right: 8rem;
+}
+.gamerNumberOfLives{
+  width: 20rem;
+  background-color: green;
+}
+.monsterNumberOfLives{
+  width: 20rem;
+  background-color: green;
+}
+.buttons{
+  background-color: white;
+  border:1px solid gray;
+  position: relative;
+  margin: auto;
+  width: 48rem;
+  border-radius: 5px;
+  margin-bottom: 3rem;
+}
+button{
+  margin: 2rem 2rem 2rem 2rem;
+  width: 5rem;
+  height: 3rem;
+  border-radius: 10px;
+  color: white;
+  font-weight: bold;
+}
+.attack{
+  background-color: red;
+}
+.specialAttack{
+  background-color: orange;
+}
+.firstAid{
+  background-color: rgb(45, 194, 45);
+}
+.giveUp{
+  border: 1px solid gray;
+  background-color: white;
+  color: black;
+}
+.log{
+  border-radius: 10px;
+  position: relative;
+  margin: auto;
+  background-color: white;
+  border: 1px solid gray;
+  width: 48rem;
+}
+.logsText{
+  color: blue;
+}
+.result{
+  position: relative;
+  margin: auto;
+  width: 45rem;
+  border: 1px solid black;
+  background-color: rgb(213, 203, 203);
+  margin-bottom: 2rem;
+}
+.gamerResut{
+  color: black;
+  background-color: rgb(190, 221, 190);
+  border: 1px solid gray;
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
+
